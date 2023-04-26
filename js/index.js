c93889f9ac2c047ebd5f7f557746ced9b6ae2962
LeugiMH <miguelhenriquegs369@gmail.com>
2023-04-26 14:15:16 -0300

    portfolio 1.0

--- js/index.js
+++ js/index.js
@@ -2,9 +2,17 @@ addEventListener("mousemove", (e) => {
     document.querySelectorAll("[data-paralax]").forEach(function(obj){
         var movingValue = obj.getAttribute("data-paralax");
         var x = (e.clientX * movingValue) / 230;
-        var y = (e.clientY * movingValue) / 200;
+        var y = (e.clientY * movingValue) / 250;
 
         obj.style.left = x+"px";
         obj.style.top = y+"px";
     });
+    document.querySelectorAll("[data-slide]").forEach(function(obj){
+            var movingValue = obj.getAttribute("data-slide");
+
+            var x = ((e.clientX * movingValue)/10);
+
+            obj.style.left = x+"px";
+        });
+    
 });
