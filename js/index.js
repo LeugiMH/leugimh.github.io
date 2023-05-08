accdaa3c060f824af74fb6050baa1c2bc35fa871
LeugiMH <miguelhenriquegs369@gmail.com>
2023-05-08 19:06:00 -0300

    glow responsifity and gyro

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//No mouse
+
 addEventListener("mousemove", (e) => {
     document.querySelectorAll("[data-paralax]").forEach(function(obj){
         var movingValue = obj.getAttribute("data-paralax");
@@ -8,11 +10,60 @@ addEventListener("mousemove", (e) => {
         obj.style.top = y+"px";
     });
     document.querySelectorAll("[data-slide]").forEach(function(obj){
-            var movingValue = obj.getAttribute("data-slide");
+        var movingValue = obj.getAttribute("data-slide");
 
-            var x = ((e.clientX * movingValue)/1);
+        var x = ((e.clientX * movingValue)) / 1;
 
-            obj.style.transform = "translateX("+x+"px)";
-        });
+        obj.style.transform = "translateX("+x+"px)";
+    });
     
+});
+//No touch
+addEventListener("touchmove", (e) => {
+    document.querySelectorAll("[data-paralax]").forEach(function(obj){
+        var movingValue = obj.getAttribute("data-paralax");
+        var x = (e.touches[0].clientX * movingValue) / 230;
+        var y = (e.touches[0].clientY * movingValue) / 250;
+
+        obj.style.left = x+"px";
+        obj.style.top = y+"px";
+    });
+    document.querySelectorAll("[data-slide]").forEach(function(obj){
+        var movingValue = obj.getAttribute("data-slide");
+
+        var x = ((e.touches[0].clientX * movingValue) / 1);
+
+        obj.style.transform = "translateX("+x+"px)";
+    });
+
+});
+//Giroscópio
+window.addEventListener("deviceorientation", (e) => {
+    const ap = document.getElementById("alpha");
+    const gb = document.getElementById("gamma");
+    const bt = document.getElementById("beta");
+    let alpha = e.alpha;
+    let gamma = e.gamma;
+    let beta = e.beta
+
+    ap.innerText = alpha;
+    gb.innerText = gamma;
+    bt.innerText = beta;
+
+    document.querySelectorAll("[data-paralax]").forEach(function(obj){
+        var movingValue = obj.getAttribute("data-paralax");
+        var x = (alpha * movingValue) / 230;
+        var y = (gamma * movingValue) / 250;
+
+        obj.style.left = x+"px";
+        obj.style.top = y+"px";
+    });
+    document.querySelectorAll("[data-slide]").forEach(function(obj){
+        var movingValue = obj.getAttribute("data-slide");
+
+        var x = ((alpha * movingValue) / 1);
+
+        obj.style.transform = "translateX("+x+"px)";
+    });
+
 });
